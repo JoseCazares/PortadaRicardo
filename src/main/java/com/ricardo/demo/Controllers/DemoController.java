893b3679f736e41a388b112c9b1820e52c7362f3
JoseCazares <josericardo1928@hotmail.com>
2021-05-20 20:23:35 -0500

    Primera subida

---
+++ src/main/java/com/ricardo/demo/Controllers/DemoController.java
@@ -0,0 +1,13 @@
+package com.ricardo.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	@GetMapping("/")
+	public String Demo() {
+		return "Portada";
+	}
+
+}
